f950a577d11ca66718b9fc95dd97b9c7d750a158
ultimate56 <220691959+ultimate56@users.noreply.github.com>
2025-09-12 17:32:45 +0500

    makes pages

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Skiper58 } from "./Components/AnimatedText";
+
+const App = () => {
+  return (
+    <div>
+      <Skiper58 />
+    </div>
+  );
+};
+
+export default App;
